a75b772d0d310c28adf72e29e3438ed6b71832e2
muleimulei <18729274217@163.com>
2017-09-09 22:29:40 +0800

    'over'

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="slider">
-    <div class="slider-group">
+  <div class="slider" ref="slider">
+    <div ref="sliderGroup" class="slider-group">
       <slot></slot>
     </div>
     <div class="dots"></div>
@@ -21,6 +21,25 @@
       interval: {
         type: Number,
         default: 4000
+      }
+    },
+    mounted () {
+      setTimeout(() => {
+        this._setSliderWidth()
+        this._initSlider()
+      })
+    },
+    methods: {
+      _setSliderWidth () {
+        this.children = this.$refs.sliderGroup.children
+        let width = 0
+        let sliderWidth = this.$refs.slider.clientWidth
+        for (let i = 0; i < this.children.length; i++){
+          let child = this.children[i]
+        }
+      },
+      _initSlider () {
+
       }
     }
   }
